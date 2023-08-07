e11128c1087bd2f006c4ec7e4dff63822d685dae
Felpo-J <nahuelprado16@gmail.com>
2023-08-07 10:42:02 -0300

    se agrega funcion fetch

--- js/script.js
+++ js/script.js
@@ -19,3 +19,12 @@ function showData(dataArray) {
 }
 
 // Escribe el código necesario para realizar el fetch al archivo con los datos y mostrar los estudiantes con la función showData
+fetch(DATA_URL)
+  .then(response => response.json())
+  .then(data => {
+    const studentsArray = data.students; 
+    showData(studentsArray); 
+  })
+  .catch(error => {
+    console.error("Error fetching data:", error);
+  });
